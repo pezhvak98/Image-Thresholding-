4e6438f67f8c65c80495a7dd1e09bad611c7bdea
Pezhvak <m.8562.m@gmail.com>
2022-06-12 14:08:26 +0430

    make base Structure

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
