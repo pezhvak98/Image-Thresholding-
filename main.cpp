a5483de100b457dc8f1c6d156a4c82a457ffbaf6
Soheil Khatibi <soheilkhatibi1377@gmail.com>
2022-06-14 17:25:21 +0430

    Format main.cpp

--- main.cpp
+++ main.cpp
@@ -7,21 +7,22 @@ using namespace std;
 using namespace cv;
 
 Mat makebinary(Mat image, int threshold) {
-
-    Mat result(image.size(),CV_8UC1);
-    for(int i = 0; i < image.rows; i++) {
-        for(int j = 0; j < image.cols; j++) {
-            if(image.at<uchar>(i,j)>threshold)
-                result.at<uchar>(i,j)=255;	//Make pixel white
-			else
-                result.at<uchar>(i,j)=0;	//Make pixel black
+    Mat result(image.size(), CV_8UC1);
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            if (image.at<uchar>(i, j) > threshold) {
+                // Make pixel white
+                result.at<uchar>(i, j) = 255;
+            } else {
+                // Make pixel black
+                result.at<uchar>(i, j) = 0;
+            }
         }
     }
     return result;
 }
 
-int main()
-{
+int main() {
     // open the first webcam plugged in the computer
     VideoCapture camera(0);
     if (!camera.isOpened()) {
@@ -29,21 +30,21 @@ int main()
         return 1;
     }
     Mat image;
-    namedWindow("Binary",WINDOW_AUTOSIZE);
+    namedWindow("Binary", WINDOW_AUTOSIZE);
     int threshold = 10;
-    createTrackbar("Threshold","Binary",&threshold,255);
-    while(1)
-    {
+    createTrackbar("Threshold", "Binary", &threshold, 255);
+    while (1) {
         camera >> image;
         Mat gray;
-        cvtColor(image,gray,COLOR_BGR2GRAY);
-        Mat binary=makebinary(gray,threshold);
+        cvtColor(image, gray, COLOR_BGR2GRAY);
+        Mat binary = makebinary(gray, threshold);
         // imshow("Original",image);
         // imshow("Grayscale",gray);
-        imshow("Binary",binary);
+        imshow("Binary", binary);
         char a = waitKey(33);
-        if(a==27) 
+        if (a == 27) {
             break;
+        }
     }
     return 0;
 }
