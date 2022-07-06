d6d743f55a13022b8111c76e6b007a6250a7cf22
fragoa <alireawz81@gmail.com>
2022-07-06 10:58:45 +0430

    clean code

--- main.cpp
+++ main.cpp
@@ -1,36 +1,61 @@
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include<opencv2/core.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/imgproc.hpp>
+
 
 using namespace std;
 using namespace cv;
 
-Mat makebinary(Mat image, int threshold)
-{
-    Mat result(image.size(), CV_8UC1);
-    for (int i = 0; i < image.rows; i++)
-    {
-        for (int j = 0; j < image.cols; j++)
-        {
-            if (image.at<uchar>(i, j) > threshold)
-            {
-                // Make pixel white
-                result.at<uchar>(i, j) = 255;
+Mat makebinary(Mat image, int threshold){
+
+        Mat result(image.size(),CV_8UC1);
+        for(int i = 0 ; i < image.rows ; i++){
+            for(int j = 0 ; j < image.cols ; j++){
+                if(image.at<uchar>(i,j)>threshold){
+                    result.at<uchar>(i,j)=255;	//Make pixel white
+                }
+                else{
+                    result.at<uchar>(i,j)=0;	//Make pixel black
             }
-            else
-            {
-                // Make pixel black
-                result.at<uchar>(i, j) = 0;
             }
         }
-    }
-    return result;
 }
 
-int main()
-{
-    Mat src = imread("mrl.jpg", 1);
+void imageThresholding()
+ {  
+    Mat image;
+    string fname;
+
+    cout << "Enter name of file:";
+    cin >> fname;
+    image = imread(fname,1);
+    namedWindow("Original",WINDOW_AUTOSIZE);
+    namedWindow("Binary",WINDOW_AUTOSIZE);
+    int threshold = 10;
+    createTrackbar("Threshold","Binary",&threshold,255);
+
+  while(1)
+  {
+
+        Mat gray;
+        cvtColor(image,gray,COLOR_BGR2GRAY);
+        Mat binary=makebinary(gray,threshold);
+        imshow("Original",image);
+        imshow("Binary",binary);
+        char a = waitKey(33);
+        if(a==27) break;
+        
+  }
+
+ }
+
+
+
+ void Convolve()
+ {
+   Mat src = imread("mrl.jpg", 1);
     float kdata[] = {0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816,
                      0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816,
                      0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816,
@@ -62,38 +87,56 @@ int main()
             dst.at<float>(i, j) = saturate_cast<float>(tmp);
         }
     }
+
     cout << "original image row " << src.rows << endl;
     cout << "original image col" << src.cols << endl;
     cout << "Convlove image  row" << dst.rows << endl;
     cout << "Convlove image col" << dst.cols << endl;
     imshow("Convlove image", dst);
-    imshow("original image", src);
     waitKey(0);
+ }
 
-    // open the first webcam plugged in the computer
-    VideoCapture camera(0);
-    if (!camera.isOpened())
+
+void ShowMenu()
     {
-        std::cerr << "ERROR: Could not open camera" << std::endl;
-        return 1;
-    }
-    Mat image;
-    namedWindow("Binary", WINDOW_AUTOSIZE);
-    int threshold = 10;
-    createTrackbar("Threshold", "Binary", &threshold, 255);
-    while (1)
+    int choice;
+    char flag;
+    cout<<"\nEnter your choice\n";
+    cout<<"\n1 -> ImageThresholding\n";
+    cout<<"\n2 -> Convlove\n";
+    while (true)
     {
-        camera >> image;
-        Mat gray;
-        cvtColor(image, gray, COLOR_BGR2GRAY);
-        Mat binary = makebinary(gray, threshold);
-        imshow("Binary", binary);
-        char a = waitKey(33);
-        if (a == 27)
+        cin>> choice;
+        switch (choice)
         {
+        case 1:
+            imageThresholding();
+            break;
+        case 2:
+            Convolve();
+            break;
+
+        default:
             break;
         }
-    }
+        
+        cout<<"\n Do you Want Continue n or y\n";
+        cin>> flag;
 
+        if (flag=='n' && flag=='N')
+        {
+        break;
+        }
+        
+
+    }
+    
+}
+
+int main()
+{
+    ShowMenu();
     return 0;
 }
+
+
