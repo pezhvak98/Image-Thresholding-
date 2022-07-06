da4d65ad4dab087624be9ac349f0090f3971721b
fragoa <alireawz81@gmail.com>
2022-07-06 11:08:22 +0430

    clean code 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include<opencv2/core.hpp>
 #include<opencv2/highgui.hpp>
-#include<opencv2/imgcodecs.hpp>
 #include<opencv2/imgproc.hpp>
 
 
@@ -10,60 +9,56 @@ using namespace cv;
 
 Mat makebinary(Mat image, int threshold){
 
-        Mat result(image.size(),CV_8UC1);
-        for(int i = 0 ; i < image.rows ; i++){
-            for(int j = 0 ; j < image.cols ; j++){
-                if(image.at<uchar>(i,j)>threshold){
-                    result.at<uchar>(i,j)=255;	//Make pixel white
-                }
-                else{
-                    result.at<uchar>(i,j)=0;	//Make pixel black
+    Mat result(image.size(),CV_8UC1);
+    for(int i = 0 ; i < image.rows ; i++){
+        for(int j = 0 ; j < image.cols ; j++){
+            if(image.at<uchar>(i,j)>threshold){
+				result.at<uchar>(i,j)=255;	//Make pixel white
             }
-            }
+			  else{
+				result.at<uchar>(i,j)=0;	//Make pixel black
         }
+        }
+    }
+    return result;
 }
 
-void imageThresholding()
- {  
+int imageThresholding()
+ { 
+    VideoCapture camera(0);
+
     Mat image;
-    string fname;
-
-    cout << "Enter name of file:";
-    cin >> fname;
-    image = imread(fname,1);
-    namedWindow("Original",WINDOW_AUTOSIZE);
-    namedWindow("Binary",WINDOW_AUTOSIZE);
+    namedWindow("Binary", WINDOW_AUTOSIZE);
     int threshold = 10;
-    createTrackbar("Threshold","Binary",&threshold,255);
-
-  while(1)
-  {
-
+    createTrackbar("Threshold", "Binary", &threshold, 255);
+    while (1)
+    {
+        camera >> image;
         Mat gray;
-        cvtColor(image,gray,COLOR_BGR2GRAY);
-        Mat binary=makebinary(gray,threshold);
-        imshow("Original",image);
-        imshow("Binary",binary);
+        cvtColor(image, gray, COLOR_BGR2GRAY);
+        Mat binary = makebinary(gray, threshold);
+        imshow("Binary", binary);
         char a = waitKey(33);
-        if(a==27) break;
-        
-  }
-
+        if (a == 27)
+        {
+            break;
+        }
+    }
+   return 0;
  }
 
 
 
  void Convolve()
  {
-   Mat src = imread("mrl.jpg", 1);
-    float kdata[] = {0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816,
-                     0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816,
-                     0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816,
-                     0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816,
-                     0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816,
-                     0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816,
-                     0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816, 0.02040816};
-    Mat kernel(7, 7, CV_32F, kdata);
+   Mat src = imread("helianthus-yellow-flower-pixabay_11863.png", 1);
+    float kdata[] = 
+    { 
+                 1.5, 1.5, 1.5, 
+                 1.5, 1.5, 1.5,
+                 1.5,1.5, 1.5
+                 };
+    Mat kernel(3, 3, CV_32F, kdata);
     Mat dst((src.rows - kernel.rows + 1), (src.cols - kernel.cols + 1), src.type());
 
     const int dx = kernel.cols / 2;
@@ -98,45 +93,45 @@ void imageThresholding()
 
 
 void ShowMenu()
+{
+  int choice;
+  char flag;
+  cout<<"\nEnter your choice\n";
+  cout<<"\n1 -> ImageThresholding\n";
+  cout<<"\n2 -> Convlove\n";
+  while (true)
+  {
+    cin>> choice;
+    switch (choice)
     {
-    int choice;
-    char flag;
-    cout<<"\nEnter your choice\n";
-    cout<<"\n1 -> ImageThresholding\n";
-    cout<<"\n2 -> Convlove\n";
-    while (true)
-    {
-        cin>> choice;
-        switch (choice)
-        {
-        case 1:
-            imageThresholding();
-            break;
-        case 2:
-            Convolve();
-            break;
+       case 1:
+          imageThresholding();
+          break;
+       case 2:
+          Convolve();
+          break;
 
-        default:
-            break;
-        }
-        
-        cout<<"\n Do you Want Continue n or y\n";
-        cin>> flag;
-
-        if (flag=='n' && flag=='N')
-        {
-        break;
-        }
-        
-
+       default:
+          break;
     }
     
+    cout<<"\n Do you Want Continue n or y\n";
+    cin>> flag;
+
+    if (flag=='n' && flag=='N')
+    {
+      break;
+    }
+    
+
+  }
+  
 }
 
 int main()
 {
-    ShowMenu();
-    return 0;
+  ShowMenu();
+  return 0;
 }
 
 
